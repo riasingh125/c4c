1ab21d70037a6b9aaaf73f1c4abff673c8c99eb3
riasingh125 <74634288+riasingh125@users.noreply.github.com>
2023-12-08 01:35:54 -0500

    Update app.js
    timestamp?

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ function postMessage() {
 
     if (message.length > 0 && message.length <= 128) {
         const messageBoard = document.getElementById('message-board');
-        
+
         // Create a new message div
         const newMessage = document.createElement('div');
 
@@ -24,21 +24,3 @@ function postMessage() {
         alert('Message must be non-empty and at most 128 characters long.');
     }
 }
-
-// Periodically check for new messages (simulate real-time updates)
-setInterval(function () {
-    // You would normally make an AJAX request here to fetch new messages from the server
-    // For simplicity, this example adds a dummy message every 5 seconds
-    const messageBoard = document.getElementById('message-board');
-    const dummyMessage = document.createElement('div');
-
-    // Get the current date and time for the dummy message
-    const timestamp = new Date();
-    const formattedTimestamp = timestamp.toLocaleString();
-
-    // Set the text content of the dummy message div to include the timestamp
-    dummyMessage.textContent = `${formattedTimestamp}: New message!`;
-
-    // Insert the dummy message at the top of the message board
-    messageBoard.insertBefore(dummyMessage, messageBoard.firstChild);
-}, 5000);
